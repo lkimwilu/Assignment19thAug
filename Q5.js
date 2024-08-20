65f3855ba33ea4d3ae383bd21b8b79a5446295b8
lkimwilu <lkimwilu@gmail.com>
2024-08-20 11:16:59 +0300

    final

--- Q5.js
+++ Q5.js
@@ -1,10 +1,21 @@
 // 5 (For Each loop): Write a JavaScript function doubleArray that takes an 
 // array of numbers as an argument and prints each number doubled using a forEach loop.
 
-function doubleArray(arr) {
-    arr.forEach(num => {
-        console.log(num * 2);
-    });
+// function doubleArray(arr) {
+//     arr.forEach(num => {
+//         console.log(num * 2);
+//     });
+// }
+
+// doubleArray([1, 2, 3, 4, 5]);
+
+
+const arr = [1, 2, 3, 4, 5];
+function doubleArray (numbers) {
+    // numbers.forEach(function (num) {
+    //     console.log(num * 2);
+    // });
+ numbers.forEach((num) => console.log(num * 2));
 }
 
-doubleArray([1, 2, 3, 4, 5]);
+doubleArray(arr);
--- Q9.js
+++ Q9.js
@@ -2,15 +2,18 @@
 // visibility of an HTML element with the id myDiv. When the function is called,
 //  it should hide the element if it's visible, and show it if it's hidden
 
-function toggleVisibility() {
+// function toggleVisibility() {
 
-    let element = document.getElementById('myDiv');
+//     let element = document.getElementById('myDiv');
 
-      if (element.style.display === 'none'){
-        element.style.display = 'block';
-    } else {
-        element.style.display = 'none';
-    }
-}
+//       if (element.style.display === 'none'){
+//         element.style.display = 'block';
+            console.log("show visibility");
+//     } else {
+//         element.style.display = 'none';
+            console.log("hide visibility");
+//     }
+// }
 
-toggleVisibility();
+// toggleVisibility();
+
